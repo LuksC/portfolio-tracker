94d35fc0e2afdd5935764b269593ed1eeaee6821
LuksC <lclemente.dev@gmail.com>
2023-05-19 10:35:40 -0300

    Borro api key

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,6 @@ import { AuthModule } from './auth/auth.module';
       load: [config],
       isGlobal: true,
       validationSchema: Joi.object({
-        API_KEY: Joi.number().required(),
         DATABASE_NAME: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
         JWT_SECRET: Joi.string().required(),
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,9 +5,6 @@ import { MongoClient } from 'mongodb';
 
 import config from '../config';
 
-const API_KEY = '12345634';
-const API_KEY_PROD = 'PROD1212121SA';
-
 @Global()
 @Module({
   imports: [
@@ -27,10 +24,6 @@ const API_KEY_PROD = 'PROD1212121SA';
   ],
   providers: [
     {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-    {
       provide: 'MONGO',
       useFactory: async (configService: ConfigType<typeof config>) => {
         const { connection, user, password, host, dbName } =
@@ -44,6 +37,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'MONGO', MongooseModule],
+  exports: ['MONGO', MongooseModule],
 })
 export class DatabaseModule {}
